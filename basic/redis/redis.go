80395f2dab4138cb0cdb0261a72539a43c82be13
ashin <lqc.ashin@gmail.com>
2019-01-21 16:04:18 +0800

    ...

--- basic/redis/redis.go
+++ basic/redis/redis.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	c, err := redis.Dial("tcp", "localhost:6379")
+	c, err := redis.Dial("tcp", "140.143.223.156:6379", redis.DialPassword("Tallqc612$"))
 	if err != nil {
 		fmt.Println("connect redis failed, ", err)
 		return
